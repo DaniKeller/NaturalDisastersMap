0c9ce387d1b55a699ad5b563d36c8dda08dd0ba3
DaniKeller <kellermdani@gmail.com>
2020-10-08 16:08:42 -0700

    fix node

--- natural-disasters-master/natural-disasters-master/app.js
+++ natural-disasters-master/natural-disasters-master/app.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static('.'));
+app.use(express.static('natural-disasters-master/natural-disasters-master/'));
 app.listen(port);
